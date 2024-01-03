2a539d0cf8678d265b7940c710ddc550b331e034
brimoAtul1520 <atul.v@brimo.in>
2024-01-03 22:56:01 +0530

    root level routing done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { Body } from './Components/Body';
 
 function App() {
   return (
     <div className="App">
-      <h1 className= "text-green-800 text-ellipsis">Helloo app</h1>
+      <Body/>
     </div>
   );
 }
---
+++ src/Components/Body.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Login } from './Login'
+import { Browse } from './Browse'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+export const Body = () => {
+
+    const appRouter = createBrowserRouter([
+        {
+            path: "/",
+            element: <Login/>
+        },
+        {
+            path: "/browse",
+            element: <Browse/>
+        }
+    ]);
+    return (
+        <div>
+            <RouterProvider router={appRouter}>
+                <Login />
+                <Browse />
+            </RouterProvider>
+        </div>
+    )
+}
