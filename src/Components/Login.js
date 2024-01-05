8238c2efd73bdd4a3cfdae78bf933b8d010d38ad
brimoAtul1520 <atul.v@brimo.in>
2024-01-05 22:59:30 +0530

    error setup done with toastyfy

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,29 +1,74 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Header } from './Header'
 import { loginvalidate, signUpvalidate } from '../Utils/FormValidate'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export const Login = () => {
     const [isSignIn, setIsSignIn] = useState(true);
-    const [isError, setIsError] = useState('');
+    const [isError, setIsError] = useState(null);
 
-    const email= useRef(null);
+    const email = useRef(null);
     const password = useRef(null);
+    const name = useRef(null);
 
-    const toogleSignIn=()=>{
+    useEffect(() => {
+        console.log("gfghjkl")
+        if(isError?.length){
+            // console.error(isError);
+            // toast(isError,{
+            //     position: "bottom-right",
+            //     autoClose: 5000,
+            //     hideProgressBar: false,
+            //     closeOnClick: true,
+            //     pauseOnHover: true,
+            //     draggable: true,
+            //     progress: undefined,
+            //     theme: "light",
+            // });
+        }
+    }, [isError])
+
+    const toogleSignIn = () => {
         setIsSignIn(!isSignIn);
     }
 
-    const handleSubmit =  async ()=>{
-        if(isSignIn){
-            let error =  loginvalidate(email?.current?.value,password?.current?.password);
+    const handleSubmit = async () => {
+        if (isSignIn) {
+            let error = loginvalidate(email?.current?.value, password?.current?.value);
 
-            if(error?.length){
+            if (error?.length) {
                 setIsError(error);
+                toast(error,{
+                    position: "bottom-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                })
             }
         }
-        // else{
-        //     let error = signUpvalidate(name,email,password)
-        //     setIsError(error);
-        // }
+        else {
+            let error = signUpvalidate(name?.current?.value, email?.current?.value, password?.current?.value)
+            if (error?.length) {
+                setIsError(error);
+                toast(error,{
+                    position: "bottom-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                })
+            }
+        }
+        // name?.current?.value='';
+        // email?.current?.value='';
+        // password?.current?.value='';
     }
 
     return (
@@ -33,9 +78,15 @@ export const Login = () => {
                 <img src='https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_large.jpg'
                     alt='Logo' />
             </div>
-            <form onSubmit={(e)=>e.preventDefault()} className="absolute w-3/12 bg-black text-white mt-36 mx-auto right-0 left-0 p-12 bg-opacity-90">
-                <h1 className="font-bold text-2xl my-2">{isSignIn?'Sign-In' : 'Sign-Up'}</h1>
-                {!isSignIn && <input type='text' placeholder='Name' className="my-2 p-3 w-full bg-gray-700"></input>}
+            <form onSubmit={(e) => e.preventDefault()} className="absolute w-3/12 bg-black text-white mt-36 mx-auto right-0 left-0 p-12 bg-opacity-90">
+                <h1 className="font-bold text-2xl my-2">{isSignIn ? 'Sign-In' : 'Sign-Up'}</h1>
+                {!isSignIn &&
+                    <input
+                        ref={name}
+                        type='text'
+                        placeholder='Name'
+                        className="my-2 p-3 w-full bg-gray-700">
+                    </input>}
                 <input
                     ref={email}
                     type='text'
@@ -48,10 +99,22 @@ export const Login = () => {
                     placeholder='Password'
                     className="my-2 p-3 w-full bg-gray-700">
                 </input>
-                <button onClick={handleSubmit} className="p-4 my-3 bg-red-700 w-full">{isSignIn?'Sign-In' : 'Sign-Up'}</button>
+                <button onClick={handleSubmit} className="p-4 my-3 bg-red-700 w-full">{isSignIn ? 'Sign-In' : 'Sign-Up'}</button>
                 <p className="cursor-pointer" onClick={toogleSignIn}>
-                    {isSignIn?'New user ? Sign Up now':'Already registered ? Sign In now'}
+                    {isSignIn ? 'New user ? Sign Up now' : 'Already registered ? Sign In now'}
                 </p>
+                <ToastContainer
+                    position="bottom-right"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"
+                />
             </form>
         </div>
     )
--- src/Utils/FormValidate.js
+++ src/Utils/FormValidate.js
@@ -8,8 +8,13 @@ export  const  loginvalidate=  (email,password)=>{
 } 
 
 
-// export default signUpvalidate = (name,email,password)=>{
-//     let validateName = loginvalidate(email,password)
-    
-//     return null;
-// }
+export const signUpvalidate = (name,email,password)=>{
+    let validateName = name?.length;
+    let validateEmail = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
+    let validatePassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+
+    if(!validateName) return "Input Name";
+    if(!validateEmail) return "InValid Email!";
+    if(!validatePassword) return "InValid Password!";
+    return '';
+}
