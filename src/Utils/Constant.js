f2913ccdb0fb58e3552f82845b4fc692d6e16b20
brimoAtul1520 <atul.v@brimo.in>
2024-01-15 02:05:40 +0530

    implement open AI key with project and have result from TMDB

--- src/Utils/Constant.js
+++ src/Utils/Constant.js
@@ -14,3 +14,32 @@ export const IMG_CDN_URL = 'https://image.tmdb.org/t/p/w500';
 
 
 export const BG_URL = 'https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_large.jpg'
+
+
+export const OPEN_AI_KEY = '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'
+
+export const MOVEI_FROM_CONST = {
+  funny: [
+      "Hera Pheri",
+      "Andaz Apna Apna",
+      "Bhagam Bhag",
+      "Golmaal: Fun Unlimited",
+      "Chupke Chupke"
+  ],
+  romantice: [
+      "Dilwale Dulhania Le Jayenge",
+      "Kuch Kuch Hota Hai",
+      "Jab Tak Hai Jaan",
+      "Hum Dil De Chuke Sanam",
+      "2 States"
+  ],
+  crime: [
+      "Gangs of Wasseypur",
+      "Drishyam",
+      "Talaash",
+      "Kahaani",
+      "Special 26"
+  ]
+};
+
+// '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';=> personal
--- src/Utils/gptSlice.js
+++ src/Utils/gptSlice.js
@@ -3,14 +3,22 @@ import { createSlice } from '@reduxjs/toolkit'
 const gptSlice = createSlice({
     name: 'gpt',
     initialState: {
-        showGptSearch: false
+        showGptSearch: false,
+        gptMoveis: null,
+        moveiName: null
     },
     reducers: {
         toggleGptSearchView: (state, actions) => {
             state.showGptSearch = !state.showGptSearch
+        },
+
+        addMoveiResult: (state, actions) => {
+            const { moveiName, gptMoveis } = actions.payload;
+            state.moveiName = moveiName;
+            state.gptMoveis = gptMoveis;
         }
     },
 })
 
-export const { toggleGptSearchView } = gptSlice.actions
+export const { toggleGptSearchView, addMoveiResult } = gptSlice.actions
 export default gptSlice.reducer
