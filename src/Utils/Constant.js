afd12087463ac47800be0698ca24afadfa8ae623
brimoAtul1520 <atul.v@brimo.in>
2024-01-15 21:39:54 +0530

    remove keys from repo setup this into env file

--- src/Utils/Constant.js
+++ src/Utils/Constant.js
@@ -5,7 +5,7 @@ export const API_OPTIONS = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhM2NmYWYzMjVmNjc1NjE4YjZmZWFjYWRkN2U0NGViNCIsInN1YiI6IjY1OWVlMDgyOGU4ZDMwMDFhODMwNTEyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XfcNiZgMQUQYcKD5ZXQA23s0JXS2XayncFJs3BZcEEY'
+    Authorization: 'Bearer '+ process.env.REACT_APP_TMDB_KEY
   }
 };
 
@@ -15,8 +15,6 @@ export const IMG_CDN_URL = 'https://image.tmdb.org/t/p/w500';
 
 export const BG_URL = 'https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_large.jpg'
 
-
-export const OPEN_AI_KEY = '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'
 
 export const MOVEI_FROM_CONST = {
   funny: [
@@ -66,4 +64,3 @@ export const MOVEI_FROM_CONST = {
   ]
 };
 
-// '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';=> personal
--- src/Utils/openAi.js
+++ src/Utils/openAi.js
@@ -1,8 +1,7 @@
 import OpenAI from 'openai';
-import { OPEN_AI_KEY } from './Constant';
 
 const openai = new OpenAI({
-  apiKey: OPEN_AI_KEY, // This is the default and can be omitted
+  apiKey: process.env.REACT_APP_OPEN_AI_KEY , 
   dangerouslyAllowBrowser:true
 });
 
